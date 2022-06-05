f6c6ee1321d26dd1175c837ec2454a4d5d3c328c
Nathan Freeman <natethegreat68.dev@gmail.com>
2022-06-05 08:59:52 -0400

    Basic source layout and relay functions.

--- dotstar_strip/dotstar_strip.c
+++ dotstar_strip/dotstar_strip.c
@@ -1,23 +1,53 @@
-/**
- * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
- *
- * SPDX-License-Identifier: BSD-3-Clause
+/*
+ * Control a Dotstar (SK9822) RGB LED strip from a Raspberry Pi Pico.
+ * Nathan Freeman (natethegreat68.dev@gmail.com)
+ * Provided under the MIT License.
  */
 
 #include "pico/stdlib.h"
 
+/*
+ * Constants.
+ */
+const uint RELAY_PIN = 16; //GP16 is the pin used to control the main relay.
+const uint SCLK_PIN = 18; //GP18 is the SCLK SPI pin for the dotstar strip.
+const uint MOSI_PIN = 19; //GP19 is the MOSI SPI pin for the dotstar strip.
+
+/*
+ * Function prototypes.
+ */
+uint setup_relay(const uint relay_pin);
+void relay_on(const uint relay_pin);
+void relay_off(const uint relay_pin);
+
+/*
+ * Main loop.
+ */
 int main() {
-#ifndef PICO_DEFAULT_LED_PIN
-#warning blink example requires a board with a regular LED
-#else
-    const uint LED_PIN = PICO_DEFAULT_LED_PIN;
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
+	// Prepare the relay.
+	const uint relay_pin = setup_relay(RELAY_PIN);
+	// Loop until power off.
     while (true) {
-        gpio_put(LED_PIN, 1);
-        sleep_ms(250);
-        gpio_put(LED_PIN, 0);
-        sleep_ms(250);
     }
-#endif
+}
+
+/*
+ * Relay-related functions.
+ */
+uint setup_relay(const uint relay_pin) {
+	// Set it up.
+	gpio_init(relay_pin);
+	gpio_set_dir(relay_pin, GPIO_OUT);
+	// Default it to off.
+	gpio_put(relay_pin, 0);
+	// Return the pin number for reference later.
+	return relay_pin;
+}
+
+void relay_on(const uint relay_pin) {
+	gpio_put(relay_pin, 1);
+}
+
+void relay_off(const uint relay_pin) {
+	gpio_put(relay_pin, 0);
 }
